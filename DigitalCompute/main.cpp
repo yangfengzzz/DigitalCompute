a933c74a86f070bb0b0f7a596ebfcb1cf42a7714
yangfengzzz <yangfengzzz@hotmail.com>
2022-08-19 09:14:53 +0800

    Initial Commit

---
+++ DigitalCompute/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  DigitalCompute
+//
+//  Created by 杨丰 on 2022/8/19.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
